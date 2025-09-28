6016069295b53efaad55d4a55941c3e2fc3135f6
Zachary Smirin <zacharysmirin@Mac.lan>
2025-09-28 12:30:43 -0400

    ads

---
+++ books.js
@@ -0,0 +1,5 @@
+const books = [
+    ["reviews/FoucaultPend.html", "Foucault's Pendulum", "Umberto Eco is my GOAT", ["Umberto Eco", "Mystery", "Politics", "Conspiracy", "Paranoia", "Fiction"]],
+    ["reviews/2666.html", "2666", "Bolaño knew ball. R.I.P. to the third leg of history", ["Roberto Bolaño", "Politics", "Mystery", "Postmodern", "Mexican", "Translated", "Fiction"]],
+    ["reviews/TLTL.html", "Too Like the Lightning", "Palmer writes some of the most ambitious science fiction of the 21st century. And some of the best", ["Ada Palmer", "Science Fiction", "Political Theory", "Fiction", "Worldbuilding", "Mystery"]]
+];
